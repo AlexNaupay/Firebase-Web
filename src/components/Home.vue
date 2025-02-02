afb004f4624a42bdf38bb42ce4750b01105e9a56
Alex Naupay <alexnaupay@gmail.com>
2025-02-02 11:43:46 -0500

    Config firebase

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 </script>
 
 <template>
-Hello
-  <RouterLink to="/about"> About </RouterLink>
+  Hello
+  <RouterLink to="/about"> About</RouterLink>
 </template>
 
 <style scoped>
